5175ba2c64fd616b7c92efc2655ac400f47b113e
0x1E7 <lewdk0x1E7@gmail.com>
2025-06-02 13:54:49 +0500

    english lang upd.

--- keyboardswitch.py
+++ keyboardswitch.py
@@ -1,27 +1,29 @@
 from time import sleep
 from subprocess import check_output
 
-def KeyboardFind() -> int:
-	DEVICE_ID = int(check_output('xinput list --id-only "AT Translated Set 2 keyboard"', shell=True))
-	print(f"Клавиатура найдена, ID = {DEVICE_ID}")
+KeyboardName = "AT Translated Set 2 keyboard" #Find your xinput device using the command "xinput list" then paste it here
+
+def KeyboardFind() -> int: #Find xinput device id by using --id-only
+	DEVICE_ID = int(check_output(f'xinput list --id-only {KeyboardName}', shell=True))
+	print(f"Keyboard found, ID = {DEVICE_ID}")
 	return DEVICE_ID
 
-def CheckState(DeviceID: int) -> int:
+def CheckState(DeviceID: int) -> int: #Checks the status of xinput device. Getting the value with grep. I'm not very good at grep yet...
 	STATE = int(check_output(f'xinput --list-props {DeviceID} | grep "Device Enabled" | grep -Eo "[0-9]+$"', shell = True))
-	print(f"Статус клавиатуры = {STATE}")
+	print(f"Keyboard state = {STATE}")
 	return STATE
 
-def RunSwitcher(DeviceState: int, DeviceID: int) -> None:
+def RunSwitcher(DeviceState: int, DeviceID: int) -> None: #Simple switch
 	if DeviceState == 1:
 		check_output(f'xinput disable {DeviceID}', shell=True)
-		print("Клавиатура ОТКЛЮЧЕНА.")
+		print("Keyboard DISABLED")
 	elif DeviceState == 0:
 		check_output(f'xinput enable {DeviceID}', shell=True)
-		print("Клавиатура ВКЛЮЧЕНА")
+		print("Keyboard ENABLED")
 	else:
-		print("Что-то пошло не так...")
+		print("Something went wrong...")
 
-if __name__ == "__main__":
+if __name__ == "__main__": #Entry point
 	DEVICE_ID = KeyboardFind()
 	DEVICE_STATE = CheckState(DEVICE_ID)
 	RunSwitcher(DEVICE_STATE, DEVICE_ID)
